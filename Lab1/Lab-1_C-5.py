7047846ef59a7db95db07fa26ca3e60cd77fe554
bomarcus <bomarcusohlsson@gmail.com>
2026-09-16 15:55:16 +0200

    split c5 and c6

--- Lab1/Lab-1_C-5.py
+++ Lab1/Lab-1_C-5.py
@@ -6,9 +6,3 @@ email = "altavista@gmail.com"
 at_sign = email.find("@")
 print(f"username is {email[0:at_sign]}")
 print(f"domain is {email[at_sign+1:]}")
-
-# replace word Java with Python in sentece. print both original and changed.
-sentence_java = "Java is great"
-sentence_python = sentence_java.replace("Java", "Python")
-print(sentence_java)
-print(sentence_python)
---
+++ Lab1/Lab-1_C-6.py
@@ -0,0 +1,7 @@
+# Lab-1_C-6
+
+# replace word Java with Python in sentece. print both original and changed.
+sentence_java = "Java is great"
+sentence_python = sentence_java.replace("Java", "Python")
+print(sentence_java)
+print(sentence_python)
