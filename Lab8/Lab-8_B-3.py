48f940cc0c35aa85a6bc0fd93d159a6fe8bbf5ee
bomarcus <bomarcusohlsson@gmail.com>
2026-09-23 14:43:50 +0200

    8-b-3

---
+++ Lab8/Lab-8_B-3.py
@@ -0,0 +1,20 @@
+# Lab-8_B-3
+
+# Add a method to Movie that returns whether the movie is highly rated. 
+# Choose a sensible rating threshold.
+
+class Movie:
+    def __init__(self, title, director, rating):
+        self.title = title
+        self.director = director
+        self.rating = rating
+
+    def rating_eval(self):
+        if self.rating > 5:
+            return "Pretty good!"
+        else:
+           return  "pretty bad!"
+    
+movie = Movie("Persona", "Ingmar Bergman", 10, )
+
+print(movie.title, movie.director, movie.rating, movie.rating_eval())
