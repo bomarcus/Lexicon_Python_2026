9baed9655836bc7595df925e1081a9f72a7eccd4
bomarcus <bomarcusohlsson@gmail.com>
2026-09-24 12:14:23 +0200

    9-a-5

---
+++ Lab9/Lab-9-A-5.py
@@ -0,0 +1,6 @@
+# Lab-9_A-5
+
+# In a comment, explain why the loop does not need to know the exact class of each object.
+
+# python is utilizing polymorphism to call the current objects send()-method
+# it only needs the method to exist and not its class.
