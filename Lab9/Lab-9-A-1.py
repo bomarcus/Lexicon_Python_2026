d14754ffc754ca60c185fcff5ba9ca558dc39ee0
bomarcus <bomarcusohlsson@gmail.com>
2026-09-24 11:47:58 +0200

    9-a-1

---
+++ Lab9/Lab-9-A-1.py
@@ -0,0 +1,12 @@
+# Lab-9_A-1
+
+# Create three classes: EmailNotification, SMSNotification and PushNotification.
+
+class EmailNotification:
+    pass
+
+class SMSNotification:
+    pass
+
+class PushNotification:
+    pass
