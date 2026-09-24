3d7af5ab2d6da077622713442e693cdb8f87fd97
bomarcus <bomarcusohlsson@gmail.com>
2026-09-24 15:10:44 +0200

    9-d-1,2

---
+++ Lab9/Lab-9-D-1.py
@@ -0,0 +1,12 @@
+# Lab-9_D-1
+
+# Create a base class User and a subclass AdminUser(User).
+
+class User:
+    def __init__(self):
+        pass
+
+
+class AdminUser(User):
+    def __init__(self):
+        super().__init__()
---
+++ Lab9/Lab-9-D-2.py
@@ -0,0 +1,15 @@
+# Lab-9_D-2
+
+# Create an AdminUser object.
+
+class User:
+    def __init__(self):
+        pass
+
+
+class AdminUser(User):
+    def __init__(self):
+        pass
+
+
+admin_user = AdminUser()
